fed50f5feb0ffda8b0179ccbd73dac4a57e5221b
Aakash <aakash.ghimire@f1soft.com>
2025-03-10 13:13:50 +0545

    example update

--- example/android/app/src/main/kotlin/com/example/m7_livelyness_detection_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.m7.poc.m7_livelyness_detection_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ example/android/app/src/main/kotlin/com/meet7/poc/m7_livelyness_detection_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.meet7.poc.m7_livelyness_detection_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
